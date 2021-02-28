9a7ccfc053ee843e70b7417c2dea5b88f516357f
William Robertson <c.william.robertson@gmail.com>
2021-02-28 19:20:54 +0100

    adds themeprovider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'fontsource-roboto'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffab40',
+    },
+    secondary: {
+      main: '#00695c',
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
