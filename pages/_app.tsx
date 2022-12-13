857e028ec2ef83d1bc82f12fbc4c3d005712f87a
William Robertson <c.william.robertson@gmail.com>
2022-12-13 12:40:42 +0100

    Update imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme'
-import createEmotionCache from '../src/createEmotionCache'
-import AppBar from '../src/components/AppBar';
+import theme from 'src/theme'
+import createEmotionCache from 'src/createEmotionCache'
+import AppBar from 'src/components/AppBar';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
