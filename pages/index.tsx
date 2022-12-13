9026a28f91c9211f7d3d0f64835aefb00869205f
William Robertson <c.william.robertson@gmail.com>
2022-12-13 12:02:12 +0100

    Loads character and error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import Error from 'next/error'
 import { Typography } from '@mui/material'
+import useCharacter from '../src/hooks/requests'
 
 export default function Home() {
+  const { character, isError, isLoading } = useCharacter()
+
+  if (isLoading) return null
+
+  if (isError) return <Error statusCode={500} />
+
   return (
     <div>
-      <Typography variant='h1' >hi there!</Typography>
+      <Typography variant="h1">hi there!</Typography>
     </div>
   )
 }
