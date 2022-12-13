363420f8e47ceb29162aed34dc8def00c45b65c8
William Robertson <c.william.robertson@gmail.com>
2022-12-13 18:05:33 +0100

    Tests setup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   testPathIgnorePatterns: ['.next/', 'node_modules/'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  testEnvironment: 'jsdom'
 };
